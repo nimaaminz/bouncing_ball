8ffe5ec98bfea97c47fe94770b188eb600a2426a
Nima <69568825+nimaaminz@users.noreply.github.com>
2021-08-17 17:31:56 +0430

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace BouncingBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(466, 549);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.ball_creator);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_mouse_up);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(277, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 571);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(473, 102);
+            this.panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 673);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Hit The Ball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BouncingBall
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch sp = new Stopwatch();
+        int mouse_last_y;
+
+        #region variable declared here . . . . 
+        //Color . . . .
+        Color BackPanel = Color.FromArgb(229, 229, 229);
+        static Color BallBrush = Color.FromArgb(16, 137, 255);
+        Color BallBorder = Color.FromArgb(35, 55, 77);
+        SolidBrush brush = new SolidBrush(BallBrush);
+        // location on page . . .
+        float PositionX;
+        float PositionY = 0;
+        float last_pos_y;
+
+        // ball details . . . 
+        int ball_width = 35;
+        int ball_height = 35;
+
+        // Physics variables . . .
+        const float a = 9.8f;
+        float z = 150;
+        float v = 0;
+        float mass = 1f;
+        float n = 0.8f;
+        float K;      // kinetic energy k = 1/2 * m * v^2 ;
+
+        // other . . . 
+        bool check = false;
+        bool check_event_call = true;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            PositionX = panel1.Width / 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            set_ball_height(100);
+        }
+
+        private void set_ball_height(float input_z)
+        {
+            z = input_z;
+            PositionY = panel1.Height - ((int)z) - ball_height;
+            //load_ball(PositionY); 
+            if (last_pos_y != PositionY)
+            {
+                panel1.Refresh();
+                last_pos_y = PositionY;
+            }
+            label3.Text = PositionY.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!(z == 0 && K < 1) && check_event_call == true)
+            {
+                //z = PositionY ; 
+                v = v + a * ((float)1f / 5f);
+                z = z - v * ((float)1f / 1.5f) /*+ (panel1.Height - z - ball_height)*/;
+
+                if (z < 0)
+                    z = 0;
+
+                //set_ball_height(load_ball(z)); 
+                set_ball_height(z);
+                //set_ball_height_not_invert(z);
+
+                if (z == 0)
+                {
+                    K = 0.5f * mass * v * v;
+                    if (K > 0)
+                    {
+                        v = v * -1 * n;
+                    }
+                }
+                label1.Text = z.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!check)
+            {
+                timer1.Enabled = true;
+                button1.Text = "Stop";
+                timer1.Start();
+                check = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button1.Text = "Start";
+                timer1.Stop();
+                check = false;
+
+            }
+            v = 0;
+        }
+        private void ball_creator(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.Clear(BackPanel);
+            e.Graphics.FillEllipse(brush, (int)PositionX, (int)PositionY, ball_width, ball_height);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            check_event_call = false;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (check_event_call == false)
+            {
+                // Y Axis . . . 
+                long t = sp.ElapsedMilliseconds;
+                int delta_Y = e.Y - mouse_last_y;
+                if (t == 0) t = 1;
+                float start_speed = delta_Y / t;
+                mouse_last_y = e.Y;
+                sp.Restart();
+                label2.Text = start_speed.ToString();
+                PositionX = e.X - ball_width / 2;
+                v = start_speed * 20;
+                set_ball_height(panel1.Height - e.Y - (ball_height / 2));
+            }
+        }
+        private void panel_mouse_up(object sender, MouseEventArgs e)
+        {
+            check_event_call = true;
+            sp.Restart();
+        }
+
+
+    }
+}
